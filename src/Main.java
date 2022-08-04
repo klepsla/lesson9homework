30462e2b9489e36bf7ef9bac0e524dbec41ff47f
klepsla <kleslava3@gmail.com>
2022-08-04 06:52:44 +0300

    Задание № 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.sql.SQLOutput;
+
+class Homework {
+        //Задание № 1
+    public static boolean LeapYear(int year) {
+        if ((year % 4 == 0) && (year % 100 != 0) || year % 400 == 0) {
+            return true;
+        }
+        return false;
+        }
+
+        //Задание № 2
+
+
+        public static void main (String[]args){
+            int year = 2020;
+            if (LeapYear(year)) {
+                System.out.println(year + " - это високосный год.");
+            } else {
+                System.out.println(year + " - это не високосный год.");
+            }
+
+
+        }
     }
-}
+
+
