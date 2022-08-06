bd979ad72bde9ba418ee305472f16e7c37b73e1b
klepsla <kleslava3@gmail.com>
2022-08-06 15:49:42 +0300

    Задание № 3 с исправлениями.

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,15 @@ import java.time.LocalDate;
 
 class Homework {
     //Задание № 1
-    public static boolean LeapYear(int year) {
+    public static boolean definitionLeapYear(int year) {
         if ((year % 4 == 0) && (year % 100 != 0) || year % 400 == 0) {
             return true;
         }
         return false;
     }
+
     //Задание № 2
-    private static void getOS (int clientOS, int clientDeviceYear) {
+    private static void definitionOS(int clientOS, int clientDeviceYear) {
         if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке...");
         } else if (clientOS == 0 && clientDeviceYear < 2015) {
@@ -21,39 +22,41 @@ class Homework {
             System.out.println("Установите облегченную версию приложения для Android по ссылке...");
         }
     }
+
     //Задание № 3
-    private static int deliveryDays (int limitation, int stepDistance, int deliveryDistance ) {
-        int numberDays = 0;
-        if (deliveryDistance > limitation) {
-            numberDays += ((deliveryDistance - limitation) / stepDistance);
-            if ((deliveryDistance - limitation) % stepDistance > 0) {
-                numberDays++;
-            }
-        }
-        return numberDays;
-    }
-    public static void main(String[] args) {
-        //к заданию № 1
-        int year = 2020;
-        if (LeapYear(year)) {
-            System.out.println(year + " - это високосный год.");
+    private static int countingDeliveryDays(int distance) {
+        if (distance < 20 && distance > 0) {
+            return 1;
+        } else if (distance < 60 && distance >= 20) {
+            return 2;
+        } else if (distance < 100 && distance >= 60) {
+            return 3;
         } else {
-            System.out.println(year + " - это не високосный год.");
+            throw new  RuntimeException("Доставка невозможна.");
         }
-        //к заданию № 2
-        int clientOS = 1;
-        int clientDeviceYear = LocalDate.now().getYear(); {
-            System.out.println("Сейчас - " + clientDeviceYear + " год ");
-            getOS (clientOS, clientDeviceYear);
-        }
-        //к заданию № 3
-        int deliveryDistance = 95;
-        int limitation = 20;
-        int stepDistance = 40;
-        int numberDays = 1;
-        numberDays += deliveryDays (limitation, stepDistance, deliveryDistance);
-        System.out.println("Потребуется дней: " + numberDays + ".");
     }
-}
+
+        public static void main (String[]args) {
+            //к заданию № 1
+            System.out.println("Задание № 1.");
+            int year = 2020;
+            if (definitionLeapYear(year)) {
+                System.out.println(year + " - это високосный год.");
+            } else
+                System.out.println(year + " - это не високосный год.");
+
+            //к заданию № 2
+            System.out.println("Задание № 2.");
+            int clientOS = 1;
+            int clientDeviceYear = LocalDate.now().getYear();
+                System.out.println("Сейчас - " + clientDeviceYear + " год ");
+                definitionOS(clientOS, clientDeviceYear);
+            //к заданию № 3
+            System.out.println("Задание № 3.");
+            int distance = 500;
+            System.out.println("Потребуется дней: " + countingDeliveryDays(distance) + ".");
+        }
+    }
+
 
 
